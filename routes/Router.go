94320af9bd425f89c272f891b4e93c4061c52181
DavidIRL <david.white0911@gmail.com>
2024-07-01 16:54:16 -0400

    create routers and DB

---
+++ routes/Router.go
@@ -0,0 +1,55 @@
+package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	controller "goRestaurantManager/controllers"
+)
+
+func FoodRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.GET("/foods", controller.GetFoods())
+	incomingRoutes.GET("/foods/:food_id", controller.GetFood())
+	incomingRoutes.POST("/foods", controller.CreateFood())
+	incomingRoutes.POST("/foods/:food_id", controller.UpdateFood())
+}
+
+func InvoiceRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.GET("/invoices", controller.GetInvoices())
+	incomingRoutes.GET("/invoices/:invoice_id", controller.GetInvoice())
+	incomingRoutes.POST("/invoices", controller.CreateInvoice())
+	incomingRoutes.POST("/invoices/:invoice_id", controller.UpdateInvoice())
+}
+
+func MenuRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.GET("/menu", controller.GetMenus())
+	incomingRoutes.GET("/menu/:menu_id", controller.GetMenu())
+	incomingRoutes.POST("/menu", controller.CreateMenu())
+	incomingRoutes.POST("/menu/:menu_id", controller.UpdateMenu())
+}
+
+func OrderRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.GET("/orders", controller.GetOrders())
+	incomingRoutes.GET("/orders/:order_id", controller.GetOrder())
+	incomingRoutes.POST("/orders", controller.CreateOrder())
+	incomingRoutes.POST("/orders/:order_id", controller.UpdateOrder())
+}
+
+func TableRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.GET("/tables", controller.GetTables())
+	incomingRoutes.GET("/tables/:table_id", controller.GetTable())
+	incomingRoutes.POST("/tables", controller.CreateTable())
+	incomingRoutes.POST("/tables/:table_id", controller.UpdateTable())
+}
+
+func UserRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.GET("/users", controller.GetUsers())
+	incomingRoutes.GET("/users/:user_id", controller.GetUser())
+	incomingRoutes.POST("/users", controller.CreateUser())
+	incomingRoutes.POST("/users/:user_id", controller.UpdateUser())
+}
+
+func OrderItemRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.GET("/orderItems", controller.GetOrderItems())
+	incomingRoutes.GET("/orderItems/:orderItem_id", controller.GetOrderItem())
+	incomingRoutes.POST("/orderItems", controller.CreateOrderItem())
+	incomingRoutes.POST("/orderItems/:orderItem_id", controller.UpdateOrderItem())
+}
