cbbc21e331424898629e9b08c58e48169f2ed996
DavidIRL <david.white0911@gmail.com>
2024-07-03 17:30:20 -0400

    update tokenHelper.go and middleware.go for auth integration

--- helpers/tokenHelper.go
+++ helpers/tokenHelper.go
@@ -1,17 +1,103 @@
 package helper
 
 import (
-
+    "fmt"
+    "time"
+    "context"
+    "log"
+    "os"
+    database "goRestaurantManager/database"
+    jwt "github.com/dgrijalva/jwt-go"
 )
 
-func GenerateAllTokens()() {
+var userCollection *mongo.Collection = database.OpenCollection(database.Client, "user")
+var SECRET_KEY string = os.Getenv("SECRET_KEY")
 
+func GenerateAllTokens(email, firstName, lastName, uid string)(signedToken, signedRefreshToken string, err error) {
+    claims := &SignedDetails{
+        Email: email,
+        First_name: firstName,
+        Last_name: lastname,
+        Uid: uid,
+        StandardClaims: jwt.StandardClaims{
+            ExpiresAt: time.Now().Local().Add(time.Hour * time.Duration(24)).Unix(),
+        },
+    }
+    
+    refreshClaims := &SignedClaims{
+        StandardClaims: jwt.StandardClaims{
+            ExpiresAt: time.Now().Local().Add(time.Hour * time.Duration(168)).Unix(),    
+        },
+    }
+
+    token, terr := jwt.NewWithClaims(jwt.SigningMethodHS256, claims).SignedString([]byte(SECRET_KEY))
+    refreshToken, rterr := jwt.NewWithClaims(jwt.SigningMethodHS256, refreshClaims).SignedString([]byte(SECRET_KEY))
+    
+    if terr != nil {
+        log.Panic(terr)
+        return
+    }
+    if rterr != nil {
+        log.Panic(rterr)
+        return
+    }
+
+    return token refreshToken, error
 }
 
-func UpdateAllTokens()() {
+func UpdateAllTokens(signedToken, signedRefreshToken, userId string) {
+    var contxt, cancel = context.WithTimeout(context.Background(), 40*time.Second)
+    var updateObj primitive.D
 
+    updateObj = append(updateObj, bson.E{"token", singedToken})
+    updateObj = append(updateObj, bson.E{"refresh_token", singedRefreshToken})
+
+    Updated_at, _ := time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+    updateObj = append(updateObj, bson.E{"updated_at", Updated_at})
+
+    upsert := true
+    filter := bson.M{"user_id": userId}
+    opt := options.UpdateOptions{
+        Upsert: &upsert
+    }
+
+    _, err := userCollection.UpdateOne(
+        contxt,
+        filter,
+        bson.D{
+            {"$set", updateObj},
+        },
+        &opt,
+    )
+        
+        defer cancel()
+        if err != nil {
+            log.Panic(err)
+            return
+        }
+        return
 }
 
-func ValidateToken()() {
+func ValidateToken(signedToken string)(claims *SignedDetails, msg string) {
+    jwt.ParseWithClaims(
+        singedToken,
+        &SignedDetails{},
+        func(token *jwt.Token)(interface{}, error) {
+            return []byte(SECRET_KEY), nil
+        },
+    )
 
+    claims, ok := token.Claims.(*SignedDetails)
+    if !ok {
+        msg = fmt.Sprintf("Token is invalid")
+        msg = err.Error()
+        return
+    }
+
+    if claims.ExpiresAt < time.Now().Local().Unix() {
+        msg = fmt.Sprintf("Token has expired")
+        msg = err.Error()
+        return
+    }
+    return claims, msg
 }
--- middleware/middleware.go
+++ middleware/middleware.go
@@ -6,6 +6,26 @@ import (
 
 func Authentication() gin.HandlerFunc {
     return func(c *gin.Context) {
+        clientToken := c.Request.Header.Get("token")
+        if clientToken == "" {
+            msg := fmt.Sprintf("No Auth header provided")
+            c.JSON(http.StatusInernalServerError, gin.H{"error": msg})
+            c.Abort()
+            return
+        }
 
+        claims, err := helper.ValidateToken(clientToken)
+        if err != "" {
+            c.JSON(http.StatusInernalServerError, gin.H{"error": err})
+            c.Abort()
+            return
+        }
+
+        c.Set("email", claims.Email)
+        c.Set("first_name", claims.First_name)
+        c.Set("last_name", claims.last_name)
+        c.Set("uid", claims.Uid)
+
+        c.Next()
     }
 }
