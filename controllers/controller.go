74b1e059f444ec3b91e163347ed3a01dc5b5c8da
DavidIRL <david.white0911@gmail.com>
2024-07-03 03:46:33 -0400

    update controller.go

--- controllers/controller.go
+++ controllers/controller.go
@@ -372,13 +372,11 @@ func UpdateInvoice() gin.HandlerFunc {
         var updateObj primitive.D
 
         if invoice.Payment_method != nil {
-            updateObj = append(updateObj, bson.E{"payment_method", invoice.Payment_method})
-            
+            updateObj = append(updateObj, bson.E{"payment_method", invoice.Payment_method})    
         }
 
         if invoice.Payment_status != nil {
             updateObj = append(updateObj, bson.E{"payment_status", invoice.Payment_status})
-
         }
 
         invoice.Updated_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
@@ -673,24 +671,121 @@ func UpdateOrder() gin.HandlerFunc {
 
 func GetTables() gin.HandlerFunc {
 	return func(c *gin.Context) {
+        var contxt, cancel = context.WithTimeout(context.Background(), 40*time.Second)
+        
+        result, err := orderCollection.Find(context.TODO(), bson.M{})
+        defer cancel()
+        if err != nil {
+            msg := fmt.Sprintf("Listing tables was unsuccessful")
+            c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+        }
 
+        var allTables []bson.M
+        if err = result.All(contxt, &allTables); err != nil {
+            log.Fatal(err)
+        }
+        c.JSON(http.StatusOK, allTables)
 	}
 }
 
 func GetTable() gin.HandlerFunc {
 	return func(c *gin.Context) {
+        var contxt, cancel = context.WithTimeout(context.Background(), 40*time.Second)
+        tableId := c.Param("_id")
+        var table models.Table
 
+        err := tableCollection.FindOne(context, bson.M{"table_id": table_id}).Decode(&table)
+        defer cancel()
+        if err != nil {
+            msg := fmt.Sprintf("Fetching table was unsuccessful")
+            c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+        }
+        c.JSON(http.StatusOK, table)            
 	}
 }
 
 func CreateTable() gin.HandlerFunc {
 	return func(c *gin.Context) {
+        var contxt, cancel = context.WithTimeout(context.Background(), 40*time.Second)
+        var table models.Table
+
+        if err := c.BindJSON(&table); err != nil {
+            c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+            return
+        }
+
+        validationErr := validate.Struct(table)
+        if validationErr != nil {
+            c.JSON(http.StatusBadRequest, gin.H{"error": validationErr.Error()})
+            return
+        }
+
+        table.Created_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+        table.Updated_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+        table.ID = primitive.NewObjectID()
+        table.Order_id = table.ID.Hex()
+
+        result, err := tableCollection.InsertOne(contxt, table)
+        if err != nil {
+            msg := fmt.Sprintf("Table creation was unsuccessful")
+            c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+            return
+        }
+        defer cancel()
+        c.JSON(http.StatusOK, result)
+
 
 	}
 }
 
 func UpdateTable() gin.HandlerFunc {
 	return func(c *gin.Context) {
+        var contxt, cancel = context.WithTimeout(context.Background(), 40*time.Second)
+        var table models.Table
+
+        var updateObj primitive.D
+
+        tableId := c.Param("table_id")
+        if err := c.BindJSON(&table); err != nil {
+            c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+            return
+        }
+
+        if table.Number_of_guests != nil {
+            updateObj = append(updateObj, bson.E{"number_of_guests", table.Number_of_guests})    
+        }
+
+        if table.Table_number != nil {
+            updateObj = append(updateObj, bson.E{"table_number", table.Table_number})    
+
+        }
+
+        table.Updated_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+        updateObj = append(updateObj, bson.E{"updated_at", table.Updated_at})
+
+        upsert := true
+        filter := bson.M{"table_id": tableId}
+        opt := options.UpdateOptions{
+            Upsert: &upsert,
+        }
+
+        result, err := tableCollection.UpdateOne(
+            contxt,
+            filter,
+            bson.D{
+                {"$set", updateObj},
+            },
+            &opt,
+        )
+
+        if err != nil {
+            msg := fmt.Sprintf("Table update was unsuccessful")
+            c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+            return
+        }
+
+        defer cancel()
+        c.JSON(http.StatusOK, result)
 
 	}
 }
@@ -759,12 +854,95 @@ func ItemByOrder(id string) (OrderItems []primitive.M, err error) {
 
 func CreateOrderItem() gin.HandlerFunc {
 	return func(c *gin.Context) {
+        var contxt, cancel = context.WithTimeout(context.Background(), 40*time.Second)
 
+        var orderItemPack orderItemPack
+        var order models.Order
+
+        if err := c.BindJSON(&orderItemPack); err != nil {
+            c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+            return
+        }
+        
+        order.Order_Date, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+
+        orderItemsToInsert := []interface{}{}
+        order.Table_id = orderItemPack.Table_id
+        order_id := OrderItemCreator(order)
+
+        for _, orderItem := range orderItemPack.Order_items {
+            orderItem.Order_id = order_id
+
+            validationErr := validate.Struct(orderItem)
+            if validationErr != nil {
+                c.JSON(http.StatusBadRequest, gin.H{"error": validationErr.Error()})
+                return
+            }
+
+            orderItem.ID = primitive.NewObjectID()
+            orderItem.Created_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+            orderItem.Updated_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+            orderItem.Order_item_id = orderItem.ID.Hex()
+            
+            var num = toFixed(*orderItem.Unit_price, 2)
+            orderItem.Unit_price = &num
+            orderItemsToInsert = append(orderItemsToInsert, orderItem)
+        }
+
+        insertedOrderItems err := orderItemCollection.InsertMany(contxt, orderItemsToInsert)
+        if err != nil {
+            log.Fatal(err)
+        }
+        defer cancel()
+        c.JSON(http.StatusOK, insertedOrderItems)
 	}
 }
 
 func UpdateOrderItem() gin.HandlerFunc {
 	return func(c *gin.Context) {
+        var contxt, cancel = context.WithTimeout(context.Background(), 40*time.Second)
 
+        var orderItem models.OrderItem
+        orderItemId := c.Param("order_item")
+        var updateObj primitive.D
+
+        if orderItem.Unit_price != nil {
+                updateObj = append(updateObj, bson.E{"unit_price", *&orderItem.Unit_price})
+        }
+
+        if orderItem.Quantity != nil {
+                updateObj = append(updateObj, bson.E{"quantity", *&orderItem.Quantity})
+        }
+
+        if orderItem.Food_id != nil {
+                updateObj = append(updateObj, bson.E{"food_id", *&orderItem.Food_id})
+        }
+
+        orderItem.Updated_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+        updateObj = append(updateObj, bson.E{"updated_at", orderItem.Updated_at})
+
+        upsert := true
+        filter := bson.M{"order_item_id": orderItemId}
+        opt := options.UpdateOptions{
+            Upsert: &upsert,
+        }
+
+        result, err := orderItemCollection.UpdateOne(
+            contxt,
+            filter,
+            bson.D{
+                {"$set", updateObj},
+            },
+            &opt,
+        )
+
+        if err != nil {
+            msg := fmt.Sprintf("Order item update was unsuccessful")
+            c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+            return
+        }
+
+        defer cancel()
+        c.JSON(http.StatusOK, result)   
 	}
 }
